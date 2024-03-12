356573d2b3d1c7a21c035cd5d1e8a0ddae79d520
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-12 19:03:05 +0000

    Change playRound to add rounds and Change playGame to play 5 round

--- scripts.js
+++ scripts.js
@@ -7,22 +7,37 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("Player wins!")
+        round++
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("Computer wins!")
+        round++
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("Player wins!")
+        round++
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("Computer wins!")
+        round++
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("Player wins!")
+        round++
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("Computer wins!")
-    } else {
-        console.log("Draw!")
+        round++
+    } else if (playerSelection === computerSelection){
+        console.log("Draw!");
+        
+        round++
     }
 }
 
+function playGame() {
+    while (round != 5) {
+        playRound(playerSelection, computerSelection);
+        console.log(round);
+    }
+}
+let round = 0;
 const computerSelection = getComputerChoice();
 const playerSelection = "roCk".toLowerCase();
 
-playRound(playerSelection, computerSelection);
+playGame();
