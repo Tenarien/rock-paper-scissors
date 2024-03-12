b56663384968ebc9b2bb13334c92cf47c21e4835
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-12 18:49:29 +0000

    Add more if statements to playRound to play just one round

--- scripts.js
+++ scripts.js
@@ -5,12 +5,24 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors")
+    if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("Player wins!")
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Computer wins!")
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Player wins!")
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("Computer wins!")
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Player wins!")
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Computer wins!")
+    } else {
+        console.log("Draw!")
     }
+}
 
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
 const playerSelection = "roCk".toLowerCase();
 
 playRound(playerSelection, computerSelection);
