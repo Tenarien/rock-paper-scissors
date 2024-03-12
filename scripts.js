4adc9e762a0da3e1d77885e553ddec64821b774d
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-12 19:40:44 +0000

    Add if statements to determine who won the game in playGame function

--- scripts.js
+++ scripts.js
@@ -7,25 +7,24 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("Player wins!")
-        round++
+        playerPoints++
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("Computer wins!")
-        round++
+        computerPoints++
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("Player wins!")
-        round++
+        playerPoints++
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("Computer wins!")
-        round++
+        computerPoints++
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("Player wins!")
-        round++
+        playerPoints++
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("Computer wins!")
-        round++
+        computerPoints++
     } else if (playerSelection === computerSelection){
         console.log("Draw!");
-        round++
     }
 }
 
@@ -34,9 +33,25 @@ function playGame() {
         playerSelection = prompt("Choose your sign").toLowerCase();
         let computerSelection = getComputerChoice()
         playRound(playerSelection, computerSelection);
-        console.log("Round: " + round);
+        round++
+        console.log("Rounds: " + round)
+        console.log("Player Points: " + playerPoints);
+        console.log("Computer Points: " + computerPoints)
+        if (round === 5 && (playerPoints > computerPoints)) {
+            console.log("Player Wins the Game!!!")
+        } else if (round === 5 && (playerPoints === computerPoints)){
+            console.log("It's a Draw!!!")
+        } else if (round === 5 && (playerPoints < computerPoints)){
+            console.log("Computer Wins the Game!!!")
+        }
     }
 }
+
+
+
+function getPlayerSelection(playerSelection) {}
+let computerPoints = 0;
+let playerPoints = 0;
 let round = 0;
 let computerSelection = getComputerChoice()
 let playerSelection = "";
